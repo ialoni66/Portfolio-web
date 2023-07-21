87d8294dbc149f5cbaa9f0cf2f87b6aa6d89bed6
ilai <ilai.aloni@gmail.com>
2023-07-21 14:55:44 -0700

    created Columns/ home page

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("zim.png")
 with col2:
     st.title("Ilai Aloni")
     content = """ Shit about me
     """
     st.info(content)
 
-# Homepage
-
+st.subheader("Below you can find some apps I have built in Python. Feel free to contact me!")
 
 # Contact me Page
 
