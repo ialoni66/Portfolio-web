69950b1eeef4997113a4fcc509f62f7032c07118
ilai <ilai.aloni@gmail.com>
2023-07-22 18:19:01 -0700

    Send Email polished

--- send_email.py
+++ send_email.py
@@ -1,19 +1,16 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "ialoni.py@gmail.com"
-password = "sxtioroyhnddfayp"
+    username = "ialoni.py@gmail.com"
+    password = "sxtioroyhnddfayp"
 
-reciever = "ialoni.py@gmail.com"
-message = """\
-Subject: Hi!
-How are you?"""
+    reciever = "ialoni.py@gmail.com"
+    context = ssl.create_default_context()
 
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, reciever, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, reciever, message)
